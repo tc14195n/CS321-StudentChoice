3cbc70ad0ba77236a924d01575965646d78b8f40
tc14195n <tj.d.carlson@gmail.com>
2020-05-03 23:18:36 -0400

    current update
    level2 needs to be optimized
    reduced numbers of zombies in level3

---
+++ StudentChoice/Assets/Scripts/wirefenceController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wirefenceController : MonoBehaviour
+{
+    public PlayerHealthController phc;
+    BoxCollider col_fence;
+    int damage;
+    float damage_timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        damage_timer = 0;
+        damage = 2;
+        col_fence = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        damage_timer += Time.deltaTime;
+        if (damage_timer % 1 == 0)
+            phc.damage(damage);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            phc.damage(damage);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        damage_timer = 0;
+    }
+}
